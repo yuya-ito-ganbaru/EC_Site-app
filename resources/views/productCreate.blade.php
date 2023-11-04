ee97f037afec5d13aee517f13db18be0c5476097
yuya-ito-ganbaru <yuya.ito1990@gmail.com>
2023-11-04 23:41:10 +0900

    商品登録ページのpost機能の追加、メッセージの表示追加

--- resources/views/productCreate.blade.php
+++ resources/views/productCreate.blade.php
@@ -12,19 +12,36 @@
 
 <body>
     <div class="productCreate-container">
+        <!------- 商品登録完了メッセージ ------->
+        @if(session('message'))
+        <div class="flash">
+            {{session('message')}}
+        </div>
+        @endif
+
+        <!------- 商品登録エラーメッセージ ------->
+        @if($errors->any())
+        <ul class="error">
+            @foreach($errors->all() as $error)
+            <li>{{$error}}</li>
+            @endforeach
+        </ul>
+        @endif
+
         <h1>商品登録ページ</h1>
-        <form>
+        <form action="{{route('productStore')}}" method="post" enctype="multipart/form-data">
+            @csrf
             <div class="title-wrap">
-                商品タイトル: <input type="text" name="name" placeholder="商品タイトル">
+                商品タイトル: <input type="text" name="name" placeholder="商品タイトル" value="{{old('name')}}">
             </div>
             <div class="img-wrap">
-                商品画像: <input type="file" name="img_path">
+                商品画像: <input type="file" name="img_path" accept="image/jpg,image/png">
             </div>
             <div class="detail-wrap">
-                商品説明: <textarea type="text" name="detail" cols="30" rows="10" placeholder="商品説明"></textarea>
+                商品説明: <textarea type="text" name="detail" cols="30" rows="10" placeholder="商品説明">{{old('detail')}}</textarea>
             </div>
             <div class="fee-wrap">
-                値段: <input type="text" name="fee" placeholder="10000">
+                値段: <input type="text" name="fee" placeholder="10000" value="{{old('fee')}}">円
             </div>
             <div class="submit-wrap">
                 <input type="submit" name="register" value="登録する">
